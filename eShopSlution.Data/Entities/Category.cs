0f5318a045fb95bd921f85330acc0aa2991a3bc1
Hoangzero8688 <140143143+Hoangzero8688@users.noreply.github.com>
2024-01-04 17:36:58 +0700

    Design Database

---
+++ eShopSlution.Data/Entities/Category.cs
@@ -0,0 +1,18 @@
+﻿using eShopSlution.Data.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShopSlution.Data.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public int SortOder { get; set; }
+        public bool IsShowOnHome { get; set; }
+        public int? ParentId { get; set; }
+        public Status Status { get; set; }
+    }
+}
